0eee002f3180ae81b5e907db5b538727ca475401
Pablo Moyano <pamoyano@miuandes.cl>
2020-03-23 20:38:50 -0300

    my first commmit

---
+++ Lab1/Lab1/First lab/First lab/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First_lab
+{
+    public class Person
+    {
+        public string name;
+        public string lastname;
+
+        public static void Lanza()
+            {Random rdnum = new Random();
+             int rn = rdnum.Next(0,3);
+            if (rn == 0)
+            { Console.WriteLine("Ha lanzado piedra"); }
+            else if (rn == 1)
+            { Console.WriteLine("Ha lanzado papel"); }
+            else
+            { Console.WriteLine("Ha lanzado tijera"); }
+
+            }
+
+    }
+}
---
+++ Lab1/Lab1/First lab/First lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace First_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.name = "Bob";
+            person.lastname = "Kunga";
+        }
+    }
+}
